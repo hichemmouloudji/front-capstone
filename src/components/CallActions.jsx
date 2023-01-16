bc7991f2589bbdcc0818bd031c6136671e467e76
hichemmouloudji <hichemmouloudji96@gmail.com>
2023-01-16 11:07:16 +0100

    Add ctc component to HomePage

--- src/components/CallActions.jsx
+++ src/components/CallActions.jsx
@@ -1,10 +1,15 @@
 // @flow 
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 const CallActions = () => {
     return (
-        <section>
-            
+        <section class="ctc">
+            <h1>30% Off This Weekend</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam praesentium voluptatibus perferendis eaque odit
+                    fugit sit minima animi temporibus saepe aut fuga, maxime vitae aliquid, tempore nihil, nobis cum dicta?
+                </p>
         </section>
     );
 };
